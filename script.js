39807125868a386397f0cf2dd58e0a64cfd87dea
eileent7129 <eat7129@nyu.edu>
2023-05-02 09:51:10 -0400

    added more videos

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// feather.replace();
+feather.replace();
+
+
+let like = document.getElementById("like");
+let dislike = document.getElementById("dislike");
+
+let trendVideos;
+let	recommendedVideos;
+let newVideo;
+like.addEventListener("click", buttonClicked);
+
+function buttonClicked() {
+	console.log("like button clicked");
+	this.style.fill = "white";
+	this.style.color = "white";
+}
+
+
+$("#like").click(buttonClicked);
+
+function buttonClicked() {
+	console.log("like button clicked");
+	$("#like").fadeOut();
+	$("#dislike").fadeOut();
+	$(".skip").fadeOut();
+
+}
